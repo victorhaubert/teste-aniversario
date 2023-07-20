ff16568e239bbba344d2228f0139d771129b4dde
Victor Haubert <95226371+victorhaubert@users.noreply.github.com>
2023-07-20 16:29:07 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+// Data de referência (29 de abril de 2021)
+const referenceDate = new Date("2021-04-29");
+
+function updateCounter() {
+    const now = new Date();
+    const timeDifference = now - referenceDate;
+
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+}
+
+// Atualiza o contador a cada segundo
+setInterval(updateCounter, 1000);
